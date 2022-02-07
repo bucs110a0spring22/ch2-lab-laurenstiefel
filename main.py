2a79011e38d0599d60a0f055d17ff9c89ea7e720
Lauren  H <98533788+laurenstiefel@users.noreply.github.com>
2022-02-07 16:39:44 +0000

    Completed the lab

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-
+import random
 #Part A
 weeks = 16
 classes = 5
 tuition = 6000
+classes_per_week=2
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
-
+cost_per_class=(cost_per_week / classes_per_week)
+print("Cost per class:",cost_per_class)
+print(classes_per_week,type(classes_per_week))
+print(weeks,type(weeks))
+print(tuition,type(tuition))
+print(cost_per_week,type(cost_per_week))
+print(cost_per_class,type(cost_per_class))
 
 #Part B
+integers=[3,7,-2,4,9]
+random.choice(integers)
+random=random.choice(integers)
+print(random)
